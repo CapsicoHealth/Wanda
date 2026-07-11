40d52c8539891d6734098b70fe46f74ebe3cc670
Laurent Hasson <410503+ldhasson@users.noreply.github.com>
2026-07-11 01:53:35 -0400

    PROMOCODE: allow for ',' and ';' for allowedDomains #115

--- src/wanda/servlets/admin/PromoCreate.java
+++ src/wanda/servlets/admin/PromoCreate.java
@@ -67,7 +67,7 @@ public class PromoCreate extends SimpleServlet
         params.put("system", new String[] {"0"}); // overwrite any system value coming in: system promotions can only be side-loaded.
         String[] allowedDomains = params.get("allowedDomains");
         if (allowedDomains != null && allowedDomains.length > 0)
-         params.put("allowedDomains", TextUtil.split(allowedDomains[0], "\\s*,\\s*"));
+         params.put("allowedDomains", TextUtil.split(allowedDomains[0].toLowerCase(), "\\s*[,;]\\s*"));
         Promo_Data p = Promo_Factory.init(params, errors);
 
         req.throwIfErrors(errors);
